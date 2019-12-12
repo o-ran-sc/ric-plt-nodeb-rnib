6674885bdb2d046b2f9e670c81115edf16ba60a7
Irina <ib565x@intl.att.com>
2019-12-12 23:08:33 +0200

    [RICPLT-2526] KeepAlive - const added
    Change-Id: Ia5c239baaad673313433c9036948a6016b6567ca
    Signed-off-by: Irina <ib565x@intl.att.com>

--- entities/e2t_instance_state.go
+++ entities/e2t_instance_state.go
@@ -21,6 +21,7 @@ package entities
 type E2TInstanceState string
 
 const (
-	Active      E2TInstanceState = "ACTIVE"
-	ToBeDeleted E2TInstanceState = "TO_BE_DELETED"
+	Active                E2TInstanceState = "ACTIVE"
+	ToBeDeleted           E2TInstanceState = "TO_BE_DELETED"
+	RoutingManagerFailure E2TInstanceState = "ROUTING_MANAGER_FAILURE"
 )
