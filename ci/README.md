57e15e8678bcb50468d406ae468d9f33991c2c9b
ronit <ronit.shechter@intl.att.com>
2020-01-27 15:16:05 +0200

    build  and publish to PackageCloud.io on merge to master branch
    Change-Id: I158a76532f439472a6f4c0ac4b8bbc87637f28f5
    Signed-off-by: ronit <ronit.shechter@intl.att.com>

---
+++ ci/README.md
@@ -0,0 +1,7 @@
+# Continuous Integration
+
+This directory contains configuration for the Jenkins CI system to
+build a docker image with the software in this repository.
+
+Additional files required to build a Docker image, for example a
+script for the CMD or ENTRYPOINT, might also be stored here.
---
+++ ci/build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+output_dir="/reader"
+
+cd ./creader 
+if [  -d $output_dir ]
+then
+    rm -r -f $output_dir
+fi
+
+make
+ls -la .$output_dir
+cd ..
+pwd
+target_dir="packages"
+project_name="rnib"
+if [ ! -d $target_dir ]
+then
+	echo "$target_dir will be created"
+    if ! mkdir -p $target_dir
+	then
+		echo "[FAIL] cannot find or create target directory: $target_dir"
+		exit 1
+	fi
+fi
+    rm -f ./$target_dir/*.*
+    mkdir -p $target_dir/DEBIAN
+    mkdir -p $target_dir/user/local/include/$project_name/
+    echo "cp -f -r .$output_dir/* $target_dir/user/local/include/$project_name"
+    cp -f -r  .$output_dir/* $target_dir/user/local/include/$project_name
+    cp ./ci/control $target_dir/DEBIAN
+    
+     ver=$(cat ./ci/package-tag.yaml | grep tag: |awk '{split($0,ver,":"); print ver[2]}'| tr -d " \t\n\r")
+     echo $project_name 
+     new_name=${project_name}_$ver
+     echo $new_names
+     dpkg-deb --build $target_dir
+     echo "mv *.deb $new_name.deb"
+     mv *.deb $new_name.deb
+     echo "deb package is: $project_name_$ver.deb"
+    ls -al *.deb
+    
