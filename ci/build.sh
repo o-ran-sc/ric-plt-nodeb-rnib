10fc6fa5295e3c7b9aed037d31fcf549fa124d51
Irina <ib565x@intl.att.com>
2020-04-23 17:25:25 +0300

    Ronit, files added to ci
    Change-Id: I1f537e363bf039bb558fb6b8ec26b37d723faaf1
    Signed-off-by: Irina <ib565x@intl.att.com>

--- ci/build.sh
+++ ci/build.sh
@@ -15,12 +15,12 @@ target_dir="packages"
 project_name="rnib"
 if [ ! -d $target_dir ]
 then
-    echo "$target_dir will be created"
+	echo "$target_dir will be created"
     if ! mkdir -p $target_dir
-    then
-        echo "[FAIL] cannot find or create target directory: $target_dir"
-        exit 1
-    fi
+	then
+		echo "[FAIL] cannot find or create target directory: $target_dir"
+		exit 1
+	fi
 fi
     rm -f ./$target_dir/*.*
     mkdir -p $target_dir/DEBIAN
@@ -28,18 +28,17 @@ fi
     echo "cp -f -r ./creader/$output_dir/* $target_dir/usr/local/include/$project_name"
     cp -f -r ./creader/$output_dir/* $target_dir/usr/local/include/$project_name
     cp ./ci/control $target_dir/DEBIAN
-
+    
      ver=$(cat ./ci/package-tag.yaml | grep tag: |awk '{split($0,ver,":"); print ver[2]}'| tr -d " \t\n\r")
-     echo $project_name
+     echo $project_name 
      new_name=${project_name}_$ver
      echo $new_names
      dpkg-deb --build $target_dir
-
-     mkdir -p exported
-
-     ls -la
-     echo "mv *.deb ./exported/$new_name.deb"
-     mv *.deb ./exported/$new_name.deb
+     mkdir -p export
+    
+     echo "mv *.deb $new_name.deb"
+     mv *.deb  $new_name.deb
      echo "deb package is: $project_name_$ver.deb"
-    cd ./exported
+    
     ls -al *.deb
+    
